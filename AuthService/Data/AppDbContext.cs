8a447aa3fe05dfb9612fbdd93990dc0b5abdc44e
Fedor Hajdu <fedorhajdu@gmail.com>
2024-10-21 22:59:11 +0100

    Add auth scaffolding

---
+++ AuthService/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using AuthService.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<User>(options)
+    {
+    }
+}
