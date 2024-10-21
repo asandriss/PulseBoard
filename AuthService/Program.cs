a739b44d1f2620d34c509e6ab7382089048359c3
Fedor Hajdu <fedorhajdu@gmail.com>
2024-10-21 23:08:34 +0100

    Configure app and build

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -1,3 +1,8 @@
+using AuthService.Data;
+using AuthService.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add authentication
+builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
+
+// Add authorization
+builder.Services.AddAuthorization();
+
+// Add Db context
+builder.Services.AddDbContext<AppDbContext>(o => o.UseSqlite("DataSource=appdata.db"));
+
+// Add Identity Core
+builder.Services.AddIdentityCore<User>()
+    .AddEntityFrameworkStores<AppDbContext>().AddApiEndpoints();
 
 var app = builder.Build();
 
@@ -16,8 +34,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// Enable Identity API
+app.MapIdentityApi<User>();
 
+app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
